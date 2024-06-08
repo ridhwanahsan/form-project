b17e5d9bab4b9ecff16c6cfdaf393479cc93d993
Ridhwan Ahsan <135047921+ridhwanahsan@users.noreply.github.com>
2024-06-09 00:34:23 +0600

    trr

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
- 
+const txt1 = document.getElementById('tbuser');
+const btn1 = document.getElementById('btn1'); const out1 = document.getElementById
+('output1');
+
+
+
+function fun1() {
+    out1.innerHTML = txt1.value;
+
+}
+btn1.addEventListener('click', fun1);
